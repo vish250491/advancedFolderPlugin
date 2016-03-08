d345bf28fd7dfb29a8bd65e201e1b356e59d6e79
Vishnu Singh <vish250491@gmail.com>
2016-03-08 18:51:12 +0530

    one test file provided

---
+++ test/control/content/app.spec.js
@@ -0,0 +1,8 @@
+describe('emulatorPlugin Content: app', function () {
+
+    describe('Buildfire service', function () {
+        it('Buildfire should exists', function () {
+            expect(true).toBeDefined();
+        });
+    });
+});
