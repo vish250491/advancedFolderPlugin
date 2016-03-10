ccffd533754ffd14952d852a38eedc8607d104b3
lakshaydulani <lakshaydulani@gmail.com>
2016-03-10 13:45:56 +0530

    lazy mimg load implemented in widget

--- widget/app.js
+++ widget/app.js
@@ -11,11 +11,12 @@
             'advancedFolderPluginWidgetFilters',
             'advancedFolderWidgetServices',
             'infinite-scroll',
-            'ngTouch'
+            'ngTouch',
+            'angularLazyImg'
         ])
         //injected ngRoute for routing
         //injected ui.bootstrap for angular bootstrap component
-        .config(['$httpProvider','$compileProvider', function ($httpProvider,$compileProvider) {
+        .config(['$httpProvider','$compileProvider','lazyImgConfigProvider', function ($httpProvider,$compileProvider,lazyImgConfigProvider) {
 
             /**
              * To make href urls safe on mobile
@@ -55,6 +56,16 @@
                 };
             }];
             $httpProvider.interceptors.push(interceptor);
+
+            var scrollable = document.querySelector('#scrolledDiv');
+            lazyImgConfigProvider.setOptions({
+                offset: 100, // how early you want to load image (default = 100)
+                errorClass: 'error', // in case of loading image failure what class should be added (default = null)
+                successClass: 'success', // in case of loading image success what class should be added (default = null)
+                onError: function(image){}, // function fired on loading error
+                onSuccess: function(image){}, // function fired on loading success
+                container: angular.element(scrollable) // if scrollable container is not $window then provide it here
+            });
 
         }])
         .run(['ViewStack', function (ViewStack) {
