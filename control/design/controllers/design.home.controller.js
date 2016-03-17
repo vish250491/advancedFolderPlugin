14bf2eb95b84600e4dea40d89a43b19fc63271e5
lakshaydulani <lakshaydulani@gmail.com>
2016-03-17 16:41:26 +0530

    fixed cleartimeout in design

--- control/design/controllers/design.home.controller.js
+++ control/design/controllers/design.home.controller.js
@@ -3,13 +3,13 @@
 (function (angular) {
     angular
         .module('advancedFolderPluginDesign')
-        .controller('DesignHomeCtrl', ['$scope', '$timeout','COLLECTIONS','DB','DEFAULT_DATA','Buildfire',
-            function ($scope, $timeout,COLLECTIONS,DB,DEFAULT_DATA,Buildfire) {
+        .controller('DesignHomeCtrl', ['$scope', '$timeout', 'COLLECTIONS', 'DB', 'DEFAULT_DATA', 'Buildfire',
+            function ($scope, $timeout, COLLECTIONS, DB, DEFAULT_DATA, Buildfire) {
                 console.log('DesignHome Controller Loaded-------------------------------------');
                 var DesignHome = this;
                 var timerDelay;
-                DesignHome.layouts = [{name: "list-layout1"}, {name: "list-layout2"}, {name: "list-layout3"}, {name: "list-layout4"},{name: "list-layout5"},{name: "list-layout6"}];
-                var advanceFolder=new DB(COLLECTIONS.advancedFolderInfo);
+                DesignHome.layouts = [{name: "list-layout1"}, {name: "list-layout2"}, {name: "list-layout3"}, {name: "list-layout4"}, {name: "list-layout5"}, {name: "list-layout6"}];
+                var advanceFolder = new DB(COLLECTIONS.advancedFolderInfo);
                 var masterInfo;
                 DesignHome.changeLayout = function (layoutName) {
                     if (layoutName && DesignHome.info.data.design) {
@@ -26,8 +26,8 @@
                             return console.err('Error:', error);
                         }
                         if (result.selectedFiles && result.selectedFiles.length) {
-                            DesignHome.info.data.design =  DesignHome.info.data.design || {};
-                            DesignHome.info.data.design.bgImage =  DesignHome.info.data.design.bgImage || {};
+                            DesignHome.info.data.design = DesignHome.info.data.design || {};
+                            DesignHome.info.data.design.bgImage = DesignHome.info.data.design.bgImage || {};
                             DesignHome.info.data.design.bgImage[imageName] = result.selectedFiles[0];
                             if (!$scope.$$phase && !$scope.$root.$$phase) {
                                 $scope.$apply();
@@ -40,8 +40,8 @@
                  * Delete the background and back to the default white background
                  * */
                 DesignHome.deleteBackground = function (imageName) {
-                    DesignHome.info.data.design =  DesignHome.info.data.design || {};
-                    DesignHome.info.data.design.bgImage =  DesignHome.info.data.design.bgImage || {};
+                    DesignHome.info.data.design = DesignHome.info.data.design || {};
+                    DesignHome.info.data.design.bgImage = DesignHome.info.data.design.bgImage || {};
                     DesignHome.info.data.design.bgImage[imageName] = undefined;
                     if (!$scope.$$phase && !$scope.$root.$$phase) {
                         $scope.$apply();
@@ -61,26 +61,26 @@
                 };
 
 
-                function init(){
-                    var success=function(data){
-                        if(data && data.data && (data.data.content || data.data.design)){
+                function init() {
+                    var success = function (data) {
+                        if (data && data.data && (data.data.content || data.data.design)) {
                             //console.log('Info got---------------');
                             updateMasterInfo(data);
-                            DesignHome.info=data;
-                            if(data.data.design && data.data.design.bgImage){
-                            //    background.loadbackground(DesignHome.info.data.design.bgImage);
+                            DesignHome.info = data;
+                            if (data.data.design && data.data.design.bgImage) {
+                                //    background.loadbackground(DesignHome.info.data.design.bgImage);
                             }
                         }
-                        else{
+                        else {
                             updateMasterInfo(DEFAULT_DATA.ADVANCED_FOLDER_INFO);
-                            DesignHome.info=DEFAULT_DATA.ADVANCED_FOLDER_INFO;
+                            DesignHome.info = DEFAULT_DATA.ADVANCED_FOLDER_INFO;
                         }
-                        console.log('Got soundcloud info successfully-----------------',data.data);
+                        console.log('Got soundcloud info successfully-----------------', data.data);
                     };
-                    var error=function(err){
-                        console.error('Error while getting data from db-------',err);
+                    var error = function (err) {
+                        console.error('Error while getting data from db-------', err);
                     };
-                    advanceFolder.get().then(success,error);
+                    advanceFolder.get().then(success, error);
                 }
 
                 init();
@@ -88,27 +88,26 @@
                 function isUnchanged(info) {
                     //console.log('info------------------------------------------',info);
                     //console.log('Master info------------------------------------------',masterInfo);
-                    return angular.equals(info,masterInfo);
+                    return angular.equals(info, masterInfo);
                 }
 
                 function updateMasterInfo(info) {
                     masterInfo = angular.copy(info);
                 }
-                function saveData(_info){
-                    var saveSuccess=function(data){
+
+                function saveData(_info) {
+                    var saveSuccess = function (data) {
                         //console.log('Data saved successfully--------------------------',data);
                     };
-                    var saveError=function(err){
-                       /* console.error('Error while saving data------------------------------',err);*/
+                    var saveError = function (err) {
+                        /* console.error('Error while saving data------------------------------',err);*/
                     };
-                    if(_info && _info.data)
-                        advanceFolder.save(_info.data).then(saveSuccess,saveError);
+                    if (_info && _info.data)
+                        advanceFolder.save(_info.data).then(saveSuccess, saveError);
                 }
 
-                function updateInfoData(_info){
-                    if (timerDelay) {
-                        clearTimeout(timerDelay);
-                    }
+                function updateInfoData(_info) {
+                    $timeout.cancel(timerDelay);
                     if (_info && _info.data && !isUnchanged(_info)) {
                         timerDelay = $timeout(function () {
                             saveData(_info);
