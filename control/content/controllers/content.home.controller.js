b5bbe6e4c9dfd6acfc0e4c18a365aecde48067bc
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-04-20 18:47:38 +0530

    Fixed issue : Re-arranging the carousel on the content does not reflect on the emulator

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -49,9 +49,22 @@
                 };
                 // this method will be called when you change the order of items
                 ContentHome.editor.onOrderChange = function (item, oldIndex, newIndex) {
-                    var temp = ContentHome.info.data.content.images[oldIndex];
-                    ContentHome.info.data.content.images[oldIndex] = ContentHome.info.data.content.images[newIndex];
-                    ContentHome.info.data.content.images[newIndex] = temp;
+                  var items = ContentHome.info.data.content.images;
+
+                  var tmp = items[oldIndex];
+
+                  if (oldIndex < newIndex) {
+                    for (var i = oldIndex + 1; i <= newIndex; i++) {
+                      items[i - 1] = items[i];
+                    }
+                  } else {
+                    for (var i = oldIndex - 1; i >= newIndex; i--) {
+                      items[i + 1] = items[i];
+                    }
+                  }
+                  items[newIndex] = tmp;
+
+                  ContentHome.info.data.content.images = items;
                     if (!$scope.$$phase)$scope.$digest();
                 };
 
@@ -124,7 +137,7 @@
                         }
                     });
 
-                }
+                };
 
                 ContentHome.pluginExist = function (instanceId) {
                     var pluginFound = false;
@@ -134,7 +147,7 @@
                         }
                     });
                     return pluginFound;
-                }
+                };
 
                 ContentHome.deleteEntity = function (obj, isFolder) {
                     var nodeData = obj.$modelValue;
