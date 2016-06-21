d0fcae1c85435f6a04e943ae5d8a9d1ce1eb7d49
Vishnu Singh <vish250491@gmail.com>
2016-06-21 15:36:12 +0530

    local image logic implemented

--- widget/directives.js
+++ widget/directives.js
@@ -155,5 +155,73 @@
                     elem.attr("src", attrs.finalSrc);
                 }
             };
+        }])
+        .directive("loadImage", ['Buildfire', function (Buildfire) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
+
+                    var _img = attrs.finalSrc;
+                    if (attrs.cropType == 'resize') {
+                        Buildfire.imageLib.local.resizeImage(_img, {
+                            width: attrs.cropWidth,
+                            height: attrs.cropHeight
+                        }, function (err, imgUrl) {
+                            _img = imgUrl;
+                            replaceImg(_img);
+                        });
+                    } else {
+                        Buildfire.imageLib.local.cropImage(_img, {
+                            width: attrs.cropWidth,
+                            height: attrs.cropHeight
+                        }, function (err, imgUrl) {
+                            _img = imgUrl;
+                            replaceImg(_img);
+                        });
+                    }
+
+                    function replaceImg(finalSrc) {
+                        var elem = $("<img>");
+                        elem[0].onload = function () {
+                            element.attr("src", finalSrc);
+                            elem.remove();
+                        };
+                        elem.attr("src", finalSrc);
+                    }
+                }
+            };
+        }])
+        .directive('backImg', ["$rootScope", function ($rootScope) {
+            return function (scope, element, attrs) {
+                attrs.$observe('backImg', function (value) {
+                    var img = '';
+                    if (value) {
+                        buildfire.imageLib.local.cropImage(value, {
+                            width: $rootScope.deviceWidth,
+                            height: $rootScope.deviceHeight
+                        }, function (err, imgUrl) {
+                            if (imgUrl) {
+                                img = imgUrl;
+                                element.attr("style", 'background:url(' + img + ') !important');
+                            } else {
+                                img = '';
+                                element.attr("style", 'background-color:white');
+                            }
+                            element.css({
+                                'background-size': 'cover'
+                            });
+                        });
+                        // img = $filter("cropImage")(value, $rootScope.deviceWidth, $rootScope.deviceHeight, true);
+                    }
+                    else {
+                        img = "";
+                        element.attr("style", 'background-color:white');
+                        element.css({
+                            'background-size': 'cover'
+                        });
+                    }
+                });
+            };
         }]);
 })(window.angular);
